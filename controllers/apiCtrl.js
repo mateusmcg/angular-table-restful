bdf8f4e385a5c2d13a47c50ab209fe162b797489
Mateus Cerqueira <mateuscg@ciandt.com>
2015-08-28 15:11:56 -0300

    Fix pages.json and apiCtrl.

--- controllers/apiCtrl.js
+++ controllers/apiCtrl.js
@@ -6,7 +6,7 @@ angular.module("angular-table-restful-example").controller("apiCtrl", ["$http", 
   function changePage(pageInfo, deferred){
   	var page = 'page' + pageInfo.pageNo + '.json';
 
-  	$http.get(page).then(function(successData){
+  	$http.get('api/' + page).then(function(successData){
   		deferred.resolve(successData);
   	}, function(errorData){
   		alert('Something went wrong with the API call :(');
