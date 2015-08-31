2c40afa6d7a18c126cabbcf444bab6f4bddb705b
Mateus Cerqueira <mateuscg@ciandt.com>
2015-08-31 09:12:12 -0300

    Fixed API pagination and added code.

--- controllers/apiCtrl.js
+++ controllers/apiCtrl.js
@@ -3,11 +3,20 @@ angular.module("angular-table-restful-example").controller("apiCtrl", ["$http", 
 
   vm.changePage = changePage;
 
+  //You must have this function in your controller in order for the API Pagination to work.
+  //Also the 'successData.data' object(each page) must have two properties despite the data.
+  //totalCount -> Total count of all items (e.g.: 3 pages with 5 items each, totalCount: 15).
+  //pageNo -> The page that the API will retreive (same value that the pageInfo.pageNo has).
   function changePage(pageInfo, deferred){
+    //The pageInfo parameters will have the following information:
+    //  -> pageNo (int)
+    //  -> pageSize (int)
+    //  -> sort (string array, e.g.: '-name' will order the list by name DESC and 'name' will order the list by name ASC)
   	var page = 'page' + pageInfo.pageNo + '.json';
 
   	$http.get('api/' + page).then(function(successData){
   		deferred.resolve(successData.data);
+      console.log(successData.data); //Also you can take a look at the api folder from this repo and see the json data.
   	}, function(errorData){
   		alert('Something went wrong with the API call :(');
   	});
