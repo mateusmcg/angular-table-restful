f618a8dfdbe82909b28ff6ec81a7495041e0bf6c
Mateus Cerqueira <mateuscg@ciandt.com>
2015-09-09 10:25:52 -0300

    Adjustments at the API Pagination example.

--- controllers/apiCtrl.js
+++ controllers/apiCtrl.js
@@ -1,37 +1,35 @@
 angular.module("angular-table-restful-example").controller("apiCtrl", ["$http", function($http) {
-  var vm = this;	
+  var vm = this;
 
-  vm.changePage = changePage;
+  vm.myTableConfig = {
+      changeEvent: tableChangeEvt
+      loadOnStartup: true //Make sure to load the table when page loads.
+  };
 
-  //You must have a function specified on the 'at-change' attribute in order for the API Pagination to work.
-  //Also the data that is passed to the 'deferred' must be an array with two more properties to work properly.
-  //totalCount -> Total count of all items (e.g.: 3 pages with 5 items each, totalCount: 15).
-  //pageNo -> The page that the API will retreive (same value that the pageInfo.pageNo has).
-  function changePage(pageInfo, deferred){
-    //The pageInfo parameters will have the following information:
-    //  -> pageNo (int)
-    //  -> pageSize (int)
-    //  -> sort (string array, e.g.: '-name' will order the list by name DESC and 'name' will order the list by name ASC)
-  	var page = 'page' + pageInfo.pageNo + '.json';
+  function tableChangeEvt(pageInfo, deferred) {
+      var page = 'page' + pageInfo.pageNo + '.json';
 
-  	$http.get('api/' + page).then(function(successData){
-      var data = prepareData(successData.data);
-  		deferred.resolve(data);
-      console.log(data); //Also you can take a look at the api folder from this repo and see the json data.
-  	}, function(errorData){
-  		alert('Something went wrong with the API call :(');
-  	});
+      $http.get('api/' + page).then(function(successData){
+          var data = prepareData(successData.data);
+          deferred.resolve(data);
+      }, function(errorData){
+          vm.apiError = true;
+      });
   }
 
-  //This function could be an interceptor, just treat the API result
+    //This function could be an interceptor, just treat the API response
   function prepareData(data){
-    var extractedData = [];
+      var extractedData = [];
 
-    extractedData = data.pageItems;
-    extractedData.totalCount = data.totalCount;
-    extractedData.pageNo = data.pageNo;
+      extractedData = data.pageItems;
 
-    return extractedData;
+      //totalCount -> Total count of all items (e.g.: 3 pages with 5 items each, totalCount: 15).
+      extractedData.totalCount = data.totalCount;
+
+      //pageNo -> The page that the API will retreive (same value that the pageInfo.pageNo has).
+      extractedData.pageNo = data.pageNo;
+
+      return extractedData;
   }
 
 }])
