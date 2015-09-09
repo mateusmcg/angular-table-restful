104f169f7265ecc886a0c43534dada6b38d16183
Mateus Cerqueira <mateuscg@ciandt.com>
2015-09-09 10:39:30 -0300

    Fix API Pagination and we are Done.

--- controllers/apiCtrl.js
+++ controllers/apiCtrl.js
@@ -1,35 +1,40 @@
+```javascript
 angular.module("angular-table-restful-example").controller("apiCtrl", ["$http", function($http) {
   var vm = this;
 
+  //Keep in mind that without the 'changeEvent' function, the API Pagination will NOT work.
   vm.myTableConfig = {
-      changeEvent: tableChangeEvt,
-      loadOnStartup: true //Make sure to load the table when page loads.
+    changeEvent: tableChangeEvt,
+    loadOnStartup: true //Make sure to load the table when page loads.
   };
 
   function tableChangeEvt(pageInfo, deferred) {
-      var page = 'page' + pageInfo.pageNo + '.json';
+    //Take a look at it and see the functionalities that you can use.
+    console.log('vm.myTableConfig', vm.myTableConfig);
 
-      $http.get('api/' + page).then(function(successData){
-          var data = prepareData(successData.data);
-          deferred.resolve(data);
-      }, function(errorData){
-          vm.apiError = true;
-      });
+    var page = 'page' + pageInfo.pageNo + '.json';
+
+    $http.get('api/' + page).then(function(successData){
+        var data = prepareData(successData.data);
+        deferred.resolve(data);
+    }, function(errorData){
+        vm.apiError = true;
+    });
   }
 
-    //This function could be an interceptor, just treat the API response
+  //This function could be an interceptor, just treat the API response
   function prepareData(data){
-      var extractedData = [];
+    var extractedData = [];
 
-      extractedData = data.pageItems;
+    extractedData = data.pageItems;
 
-      //totalCount -> Total count of all items (e.g.: 3 pages with 5 items each, totalCount: 15).
-      extractedData.totalCount = data.totalCount;
+    //totalCount -> Total count of all items (e.g.: 3 pages with 5 items each, totalCount: 15).
+    extractedData.totalCount = data.totalCount;
 
-      //pageNo -> The page that the API will retreive (same value that the pageInfo.pageNo has).
-      extractedData.pageNo = data.pageNo;
+    //pageNo -> The page that the API will retreive (same value that the pageInfo.pageNo has).
+    extractedData.pageNo = data.pageNo;
 
-      return extractedData;
+    return extractedData;
   }
 
 }])
